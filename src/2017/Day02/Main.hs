d1ad89b4a93e4ffa663769a7c79c65b2116a75c0
Joshua Clayton <joshua.clayton@gmail.com>
2017-12-03 16:05:10 -0500

    Day 2.2

--- src/2017/Day02/Main.hs
+++ src/2017/Day02/Main.hs
@@ -2,6 +2,7 @@
 -- stack --install-ghc runghc -- -isrc
 import AdventOfCode
 import Data.Char
+import Data.Maybe
 import Text.Megaparsec
 import Text.Megaparsec.Lexer as L
 import Text.Megaparsec.Text
@@ -14,7 +15,18 @@ process = sum . map checksum
 
 checksum :: [Int] -> Int
 checksum [] = 0
-checksum xs = maximum xs - minimum xs
+checksum xs = head $ Data.Maybe.catMaybes evenSplits
+  where
+    evenSplits = map evenSplit pairs
+    pairs = [(a, b) | a <- xs, b <- reverse xs]
+
+evenSplit :: (Int, Int) -> Maybe Int
+evenSplit (a, b) =
+    if a == b
+        then Nothing
+        else case a `divMod` b of
+                 (res, 0) -> Just res
+                 _ -> Nothing
 
 inputParser :: Parser [[Int]]
 inputParser = lineParser `sepBy` newline

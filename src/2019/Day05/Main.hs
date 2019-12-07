e8e70571da13263653546f545c45ed1a30eedba9
Joshua Clayton <joshua.clayton@gmail.com>
2019-12-07 10:06:22 -0500

    WIP: does not work

--- src/2019/Day05/Main.hs
+++ src/2019/Day05/Main.hs
@@ -24,14 +24,24 @@ data Intcode
                Integer
     | Input Parameter
     | Output Parameter
+    | JumpIfTrue Parameter
+                 Parameter
+    | JumpIfFalse Parameter
+                  Parameter
+    | LessThan Parameter
+               Parameter
+               Integer
+    | Equals Parameter
+             Parameter
+             Integer
     | Halt
     deriving (Show, Eq)
 
 data Program =
     Program (Integer, Maybe Integer)
-            [Integer]
-            Intcode
-            [Integer]
+            ![Integer]
+            !Intcode
+            ![Integer]
     deriving (Show)
 
 intcodeToIntegers :: Intcode -> [Integer]
@@ -40,6 +50,27 @@ intcodeToIntegers (Input (Position p1)) = [3, p1]
 intcodeToIntegers (Input (Immediate p1)) = [103, p1]
 intcodeToIntegers (Output (Position p1)) = [4, p1]
 intcodeToIntegers (Output (Immediate p1)) = [104, p1]
+intcodeToIntegers (JumpIfTrue (Position p1) (Position p2)) = [5, p1, p2]
+intcodeToIntegers (JumpIfTrue (Position p1) (Immediate p2)) = [105, p1, p2]
+intcodeToIntegers (JumpIfTrue (Immediate p1) (Position p2)) = [1005, p1, p2]
+intcodeToIntegers (JumpIfTrue (Immediate p1) (Immediate p2)) = [1105, p1, p2]
+intcodeToIntegers (JumpIfFalse (Position p1) (Position p2)) = [6, p1, p2]
+intcodeToIntegers (JumpIfFalse (Position p1) (Immediate p2)) = [106, p1, p2]
+intcodeToIntegers (JumpIfFalse (Immediate p1) (Position p2)) = [1006, p1, p2]
+intcodeToIntegers (JumpIfFalse (Immediate p1) (Immediate p2)) = [1106, p1, p2]
+intcodeToIntegers (LessThan (Position p1) (Position p2) p3) =
+    [00007, p1, p2, p3]
+intcodeToIntegers (LessThan (Immediate p1) (Position p2) p3) =
+    [00107, p1, p2, p3]
+intcodeToIntegers (LessThan (Position p1) (Immediate p2) p3) =
+    [01007, p1, p2, p3]
+intcodeToIntegers (LessThan (Immediate p1) (Immediate p2) p3) =
+    [01107, p1, p2, p3]
+intcodeToIntegers (Equals (Position p1) (Position p2) p3) = [00008, p1, p2, p3]
+intcodeToIntegers (Equals (Immediate p1) (Position p2) p3) = [00108, p1, p2, p3]
+intcodeToIntegers (Equals (Position p1) (Immediate p2) p3) = [01008, p1, p2, p3]
+intcodeToIntegers (Equals (Immediate p1) (Immediate p2) p3) =
+    [01108, p1, p2, p3]
 intcodeToIntegers (Add (Position p1) (Position p2) p3) = [00001, p1, p2, p3]
 intcodeToIntegers (Add (Immediate p1) (Position p2) p3) = [00101, p1, p2, p3]
 intcodeToIntegers (Add (Position p1) (Immediate p2) p3) = [01001, p1, p2, p3]
@@ -54,11 +85,42 @@ intcodeToIntegers (Multiply (Immediate p1) (Immediate p2) p3) =
     [01102, p1, p2, p3]
 
 intcodeLength :: Intcode -> Int
+intcodeLength JumpIfTrue {} = 3
+intcodeLength JumpIfFalse {} = 3
 intcodeLength Add {} = 4
 intcodeLength Multiply {} = 4
+intcodeLength LessThan {} = 4
+intcodeLength Equals {} = 4
 intcodeLength (Input _) = 2
 intcodeLength (Output _) = 2
 intcodeLength Halt = 1
+
+replaceParameter :: Parameter -> Integer -> Parameter
+replaceParameter (Immediate _) v = Immediate v
+replaceParameter (Position _) v = Position v
+
+changeIntcodePosition :: Intcode -> Int -> Integer -> Intcode
+changeIntcodePosition Halt _ _ = error "strange"
+changeIntcodePosition (Add p1 p2 p3) 1 v = Add (replaceParameter p1 v) p2 p3
+changeIntcodePosition (Add p1 p2 p3) 2 v = Add p1 (replaceParameter p2 v) p3
+changeIntcodePosition (Add p1 p2 p3) 3 v = Add p1 p2 v
+changeIntcodePosition (Multiply p1 p2 p3) 1 v =
+    Multiply (replaceParameter p1 v) p2 p3
+changeIntcodePosition (Multiply p1 p2 p3) 2 v =
+    Multiply p1 (replaceParameter p2 v) p3
+changeIntcodePosition (Multiply p1 p2 p3) 3 v = Multiply p1 p2 v
+changeIntcodePosition (LessThan p1 p2 p3) 1 v =
+    LessThan (replaceParameter p1 v) p2 p3
+changeIntcodePosition (LessThan p1 p2 p3) 2 v =
+    LessThan p1 (replaceParameter p2 v) p3
+changeIntcodePosition (LessThan p1 p2 p3) 3 v = LessThan p1 p2 v
+changeIntcodePosition (Equals p1 p2 p3) 1 v =
+    Equals (replaceParameter p1 v) p2 p3
+changeIntcodePosition (Equals p1 p2 p3) 2 v =
+    Equals p1 (replaceParameter p2 v) p3
+changeIntcodePosition (Equals p1 p2 p3) 3 v = Equals p1 p2 v
+changeIntcodePosition (Input p1) 1 v = Input (replaceParameter p1 v)
+changeIntcodePosition (Output p1) 1 v = Output (replaceParameter p1 v)
 
 getValueAtPosition :: Integer -> Program -> Integer
 getValueAtPosition position (Program initialValue previousIntcodes currentIntcode rest)
@@ -79,8 +141,17 @@ setValueAtPosition position newValue (Program initialValue previousIntcodes curr
             (replace position' newValue previousIntcodes)
             currentIntcode
             rest
-    | position' >= positionsBeforeCurrent && position' < offsetAfterIntcode =
-        error "replacing value inside self"
+    | position' == positionsBeforeCurrent =
+        error "changing the entire type of intcode"
+    | position' > positionsBeforeCurrent && position' < offsetAfterIntcode =
+        Program
+            initialValue
+            previousIntcodes
+            (changeIntcodePosition
+                 currentIntcode
+                 (position' - positionsBeforeCurrent)
+                 newValue)
+            rest
     | otherwise =
         Program
             initialValue
@@ -103,8 +174,10 @@ initializeProgram initialValue =
 main :: IO ()
 main = runProgram process integersParser
 
+starting = 5
+
 process :: [Integer] -> Integer
-process = output . initializeProgram 1
+process = output . initializeProgram starting
 
 output :: Program -> Integer
 output (Program (_, Just v) _ _ _) = v
@@ -120,34 +193,71 @@ setOutput :: Program -> Integer -> Program
 setOutput (Program (initialValue, _) intcodes currentIntcode rest) newValue =
     Program (initialValue, Just newValue) intcodes currentIntcode rest
 
-executeIntcode' :: Intcode -> Program -> Program
+setInstructionPointerTo :: Integer -> Program -> Program
+setInstructionPointerTo position (Program initialValue intcodes currentIntcode rest) =
+    Program initialValue intcodes' currentIntcode' rest'
+  where
+    programToIntegers = intcodes ++ intcodeToIntegers currentIntcode ++ rest
+    (intcodes', remainder) = splitAt position' programToIntegers
+    (currentIntcode', rest') = parseIntcode remainder
+    position' = fromInteger position
+
+executeIntcode' :: Intcode -> Program -> (Program, Bool)
 executeIntcode' intcode program@(Program initialValue intcodes currentIntcode rest) =
-    case currentIntcode of
-        Input (Position p1) -> setValueAtPosition p1 (fst initialValue) program
-        Output p1 -> setOutput program $ valueFor p1 program
-        Halt -> program
+    case intcode of
+        Input (Position p1) ->
+            (setValueAtPosition p1 (fst initialValue) program, True)
+        Output p1 -> (setOutput program $ valueFor p1 program, True)
+        Halt -> (program, True)
         Add p1 p2 p3 ->
-            setValueAtPosition
-                p3
-                (valueFor p1 program + valueFor p2 program)
-                program
+            ( setValueAtPosition
+                  p3
+                  (valueFor p1 program + valueFor p2 program)
+                  program
+            , True)
         Multiply p1 p2 p3 ->
-            setValueAtPosition
-                p3
-                (valueFor p1 program * valueFor p2 program)
-                program
+            ( setValueAtPosition
+                  p3
+                  (valueFor p1 program * valueFor p2 program)
+                  program
+            , True)
+        JumpIfTrue p1 p2 ->
+            if valueFor p1 program /= 0
+                then ( setInstructionPointerTo (valueFor p2 program) program
+                     , False)
+                else (program, True)
+        JumpIfFalse p1 p2 ->
+            if valueFor p1 program == 0
+                then ( setInstructionPointerTo (valueFor p2 program) program
+                     , False)
+                else (program, True)
+        LessThan p1 p2 p3 ->
+            ( if valueFor p1 program < valueFor p2 program
+                  then setValueAtPosition p3 1 program
+                  else setValueAtPosition p3 0 program
+            , True)
+        Equals p1 p2 p3 ->
+            ( if valueFor p1 program == valueFor p2 program
+                  then setValueAtPosition p3 1 program
+                  else setValueAtPosition p3 0 program
+            , True)
 
 executeIntcode :: Program -> Program
 executeIntcode program@(Program _ _ code _) =
+    t "new program" $
     Program
         initialValue
         (intcodes ++ intcodeToIntegers currentIntcode)
         newIntcode
         rest'
   where
-    (Program initialValue intcodes currentIntcode rest) =
+    ((Program initialValue intcodes currentIntcode rest), continue) =
         executeIntcode' code program
-    (newIntcode, rest') = parseIntcode rest
+    (newIntcode, rest') =
+        case (continue, rest) of
+            (True, []) -> (currentIntcode, rest)
+            (True, _) -> parseIntcode rest
+            (False, _) -> (currentIntcode, rest)
 
 programHalts :: Program -> Bool
 programHalts (Program (_, Just _) _ Halt _) = True
@@ -171,5 +281,21 @@ parseIntcode integers =
                 00102 -> (Multiply (Immediate p1) (Position p2) p3, rest)
                 01002 -> (Multiply (Position p1) (Immediate p2) p3, rest)
                 00002 -> (Multiply (Position p1) (Position p2) p3, rest)
+                00005 -> (JumpIfTrue (Position p1) (Position p2), p3 : rest)
+                00105 -> (JumpIfTrue (Position p1) (Immediate p2), p3 : rest)
+                01005 -> (JumpIfTrue (Immediate p1) (Position p2), p3 : rest)
+                01105 -> (JumpIfTrue (Immediate p1) (Immediate p2), p3 : rest)
+                00006 -> (JumpIfFalse (Position p1) (Position p2), p3 : rest)
+                00106 -> (JumpIfFalse (Position p1) (Immediate p2), p3 : rest)
+                01006 -> (JumpIfFalse (Immediate p1) (Position p2), p3 : rest)
+                01106 -> (JumpIfFalse (Immediate p1) (Immediate p2), p3 : rest)
+                01107 -> (LessThan (Immediate p1) (Immediate p2) p3, rest)
+                00107 -> (LessThan (Immediate p1) (Position p2) p3, rest)
+                01007 -> (LessThan (Position p1) (Immediate p2) p3, rest)
+                00007 -> (LessThan (Position p1) (Position p2) p3, rest)
+                01108 -> (Equals (Immediate p1) (Immediate p2) p3, rest)
+                00108 -> (Equals (Immediate p1) (Position p2) p3, rest)
+                01008 -> (Equals (Position p1) (Immediate p2) p3, rest)
+                00008 -> (Equals (Position p1) (Position p2) p3, rest)
                 _ -> error $ "unexpected intcode value: " ++ show n
-        _ -> error "unable to parse intcode"
+        _ -> error $ "unable to parse intcode: " ++ show integers
